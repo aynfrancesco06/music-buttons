959f4579e85c7c677d50905cf3b44f8608b6ea98
aynfrancesco06 <56792014+aynfrancesco06@users.noreply.github.com>
2021-01-24 20:03:10 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let a = document.getElementById("letter_A");
+let b = document.getElementById("letter_B");
+let c = document.getElementById("letter_C");
+let d = document.getElementById("letter_D");
+let e = document.getElementById("letter_E");
+let f = document.getElementById("letter_F");
+
+
+function playA() {
+    a.play();
+}
+
+function playB() {
+    b.play();
+}
+
+function playC() {
+    c.play();
+}
+
+function playD() {
+    d.play();
+}
+
+function playE() {
+    e.play();
+}
+
+function playF() {
+    f.play();
+}
